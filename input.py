a8a27d0c74245024b0e83117f8aaf2b6e92c76e2
Quethly-12 <125228980+Quethly-12@users.noreply.github.com>
2024-03-05 15:34:56 +0800

    First Commit

---
+++ input.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter 1st number: "))
+
+num2 = int(input("Enter 2nd number: "))
+int(num2)
+
+sum = num1 + num2
+
+print(f"The sum of {num1} and {num2} is: {sum}")
---
+++ output.py
@@ -0,0 +1,9 @@
+#variable declaration
+x = 25
+message = "Hi po!"
+ratio = 3.14
+#checkin the type of data with type() method.
+print(type(message))
+print(type(ratio))
+print(type(x))
+print(f"{message} My name is Que. I am {x} years old")
